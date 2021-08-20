cf560b5f235b8fe5e06eb7c52f1b41b7b17769e5
Jon Sawyers <jon-sawyers@lambdastudents.com>
2021-08-20 05:56:57 -0500

    added routes and links and built out the form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from "react";
 
+import { Route, Link } from "react-router-dom";
+
+import Pizza from "./Pizza"
+
+
+
 const App = () => {
+
+
+
+
   return (
-    <>
+    <div className = 'App'>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <nav>
+        <Link to='/'>Home</Link>
+      </nav>
+      <nav>  
+        <Link to='/help'>Help</Link>
+        <Route exact path='/'>
+        
+
+        </Route>
+        
+        
+      </nav>
+      <div>
+        <Link to='/pizza'>
+          Pizza!
+        </Link>
+        <Route path= '/pizza'>
+          <Pizza />
+        </Route>
+        
+      </div>
+      
+
+    
+    </div>
+
+   
   );
 };
 export default App;
